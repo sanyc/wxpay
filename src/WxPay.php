60d399adec1d2ad2d50d20a3525f5ffbb3bc1f9c
sanyc <niexiaojiang@sina.com>
2016-12-13 12:23:49 +0800

    优化

--- src/WxPay.php
+++ src/WxPay.php
@@ -15,20 +15,39 @@ use Sanyc\WxPay\sdk\NativePay;
 class WxPay 
 {
     private $url = '';
+    const TIME_EXPIRE = 600;
+    const BODY = '双辉旅程网';
+
     public function __construct(array $payment_info = [])
     {
         $notify = new NativePay();
         $input = new WxPayUnifiedOrder();
-        $input->SetBody($payment_info['body']);
-        $input->SetAttach($payment_info['attach']);
+        if (isset($payment_info['body'])) {
+            $input->SetBody($payment_info['body']);
+        }else{
+            $input->SetBody(self::BODY);
+        }
+        if (isset($payment_info['attach'])) {
+            $input->SetAttach($payment_info['attach']);
+        }
         $input->SetOut_trade_no(WxPayConfig::getConfig('MCHID').date("YmdHis"));
         $input->SetTotal_fee($payment_info['total']);
         $input->SetTime_start(date("YmdHis"));
-        $input->SetTime_expire(date("YmdHis", time() + 600));
+        $input->SetTime_expire(date("YmdHis", time() + self::TIME_EXPIRE));
         $input->SetGoods_tag("pay");
-        $input->SetNotify_url($payment_info['notify_url']);
-        $input->SetTrade_type("NATIVE");
-        $input->SetProduct_id($payment_info['product_id']);
+        if (isset($payment_info['notify_url'])) {
+            $input->SetNotify_url($payment_info['notify_url']);
+        } 
+        if (isset($payment_info['native'])) {
+            $input->SetTrade_type($payment_info['native']);
+        }else{
+            $input->SetTrade_type("NATIVE");
+        }
+        if (isset($payment_info['product_id'])) {
+            $input->SetProduct_id($payment_info['product_id']);
+        }else{
+            $input->SetProduct_id('100');
+        }
         //$input->SetOut_trade_no("sjz01234567892");
         $result = $notify->GetPayUrl($input);
         $this->url = $result['code_url'];
