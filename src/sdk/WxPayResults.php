19590d4fade5a01e29994fcc4275dded7e45e9dc
sanyc <niexiaojiang@sina.com>
2016-12-16 10:24:34 +0800

    签名错误提示

--- src/sdk/WxPayResults.php
+++ src/sdk/WxPayResults.php
@@ -17,14 +17,14 @@ class WxPayResults extends WxPayDataBase
     {
         //fix异常
         if(!$this->IsSignSet()){
-            throw new WxPayException("签名错误！");
+            throw new WxPayException("签名错误a！");
         }
         
         $sign = $this->MakeSign();
         if($this->GetSign() == $sign){
             return true;
         }
-        throw new WxPayException("签名错误！");
+        throw new WxPayException("签名错误b！");
     }
     
     /**
